3b4dbfc00f4748baa074b6b2ffdb06f8abbeefc7
Letmeouted <1002726239@qq.com>
2021-12-01 14:36:07 +0800

    Main process add Window method

--- main.js
+++ main.js
@@ -35,7 +35,35 @@ function createWindow () {
   // Emitted when the window is closed.
   mainWindow.on('closed', function () { mainWindow = null })
 }
-
+const path = require('path')
+const url = require('url')
+const ipc = require('electron').ipcMain
+let newwin=null;
+ipc.on('tab3',()=>
+{
+    if(newwin){
+      newwin.show()
+    }else{
+      newwin = new BrowserWindow({
+        width: 600, 
+        height: 300,
+        autoHideMenuBar: true,
+        frame: true,
+        transparent: false,
+        resizable: true,
+        x:1000,
+        y:300,
+        parent: mainWindow, //mainWindow是主窗口
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation:false,//slove the alarm problem
+          enableRemoteModule:true //ensure renderer.js can be used normally require('electron').remote
+        }
+    })
+    newwin.loadURL(path.join('file:',__dirname,'new.html')); //new.html是新开窗口的渲染进程
+    newwin.on('closed',()=>{newwin = null})
+    }
+})
 app.on('ready', function() { createWindow() })
 
 // Quit when all windows are closed.
