962718f78a611f4bb5ff554d156b7650b1787fa2
王华锋 <huafeng.wang@cubicise.com>
2022-12-02 14:29:06 +0800

    perf: optimize log panel

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
  * @Email: 1002726239@qq.com
  * @FilePath: \SMNFiler\SMNFiler\main.js
  */
-const {app, BrowserWindow,globalShortcut} = require('electron')
+var { app, BrowserWindow, globalShortcut } = require("electron");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const path = require("path");
+const fs = require("fs");
+const url = require("url");
 
-function createWindow () {
+let mainWindow;
+
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 680,
@@ -20,78 +24,57 @@ function createWindow () {
     alwaysOnTop: false, // [true] had bad experience on windows
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation:false,//slove the alarm problem
-      enableRemoteModule:true //ensure renderer.js can be used normally require('electron').remote
-    }
-  })
+      contextIsolation: false, //slove the alarm problem
+      enableRemoteModule: true, //ensure renderer.js can be used normally require('electron').remote
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () { mainWindow = null })
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+  mainWindow.on("focus", () => {
+    globalShortcut.register("CommandOrControl+F", function () {
+      if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.send("on-find", "");
+      }
+    });
+  });
+  mainWindow.on("blur", () => {
+    globalShortcut.unregister("CommandOrControl+F");
+  });
+
 }
-const path = require('path')
-const fs = require('fs')
-const url = require('url')
-const ipc = require('electron').ipcMain
-let newwin=null;
-ipc.on('tab3',()=>
-{
-    if(newwin){
-      newwin.show()
-    }else{
-      newwin = new BrowserWindow({
-        width: 600, 
-        height: 300,
-        autoHideMenuBar: true,
-        frame: true,
-        transparent: false,
-        resizable: true,
-        x:1000,
-        y:300,
-        parent: mainWindow, //mainWindow是主窗口
-        webPreferences: {
-          nodeIntegration: true,
-          contextIsolation:false,//slove the alarm problem
-          enableRemoteModule:true //ensure renderer.js can be used normally require('electron').remote
-        }
-    })
-    newwin.loadURL(path.join('file:',__dirname,'new.html')); //new.html是新开窗口的渲染进程
-    newwin.on('closed',()=>{
-      newwin = null;
-    })
-    newwin.on('focus',()=>{
-      globalShortcut.register('CommandOrControl+F', function () {
-        if (newwin && newwin.webContents) {
-          newwin.webContents.send('on-find', '')
-        }
-      })
-    })
-    newwin.on('blur', () => {
-      globalShortcut.unregister('CommandOrControl+F')
-    })
-    }
-})
-app.on('ready', function() { createWindow() })
+// const ipc = require("electron").ipcMain;
+// ipc.on("tab3", () => {
+
+// });
+app.on("ready", function () {
+  createWindow();
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  const { app } =require('electron')
-  var filepath = path.join(app.getPath('temp'))
-  var filename = "SMNFiler.log"
-  fs.unlink(path.join(filepath,filename),function(err){
-    if(err){
-      console.log("An error ocurred updating the file" + err.message)
-      return
+app.on("window-all-closed", function () {
+  const { app } = require("electron");
+  var filepath = path.join(app.getPath("temp"));
+  var filename = "SMNFiler.log";
+  fs.unlink(path.join(filepath, filename), function (err) {
+    if (err) {
+      console.log("An error ocurred updating the file" + err.message);
+      return;
     }
-    console.log("File successfully deleted")
-  }) 
-  app.quit() 
-  globalShortcut.unregister('CommandOrControl+F')
-})
+    console.log("File successfully deleted");
+  });
+  app.quit();
+  globalShortcut.unregister("CommandOrControl+F");
+});
 
-app.on('activate', function () { if (mainWindow === null) createWindow() })
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
