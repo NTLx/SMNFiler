b859fe4bf8f8d4c47ff651e547696b342aa2ca0e
王华锋 <huafeng.wang@cubicise.com>
2023-07-25 02:33:16 +0000

    feat: add configured code to realize packaging operation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import { install } from "@vueuse/core";
+const debounce = (fn, delay) => {
+  let timer = null;
+  return function () {
+    let context = this;
+    let args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+};
+process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 16);
+    super(callback);
+  }
+};
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(install);
+app.use(ElementPlus);
+app.mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,64 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        // 如果你在开发环境中使用 webpack 5，则需要使用以下语句:
+        // 'path': require.resolve('path-browserify')
+        path: require.resolve("path-browserify"),
+        // fs: false,
+        // os: false,
+        // electron: false,
+      },
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      externals: [
+        "electron-log",
+        "@electron/remote",
+        "node-xlsx",
+        "xlsx",
+        "officegen",
+      ],
+      nodeModulesPath: ["../../node_modules", "./node_modules"],
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        productName: "SMNFiler",
+        appId: "com.smnfiler.app",
+        dmg: {
+          title: "SMNFiler",
+          icon: "./src/assets/app256x256.icns",
+        },
+        mac: {
+          category: "public.app-category.developer-tools",
+          icon: "./src/assets/app256x256.icns",
+          extendInfo: {
+            LSUIElement: 0,
+          },
+          target: ["dmg"],
+          // eslint-disable-next-line no-template-curly-in-string
+          artifactName: "${productName}.v${version}.MacOS.${ext}",
+        },
+        win: {
+          icon: "./src/assets/app.ico",
+          // eslint-disable-next-line no-template-curly-in-string
+          artifactName: "${productName}.v${version}.Win_Portable.${ext}",
+          target: [
+            {
+              target: "portable",
+              arch: ["ia32"],
+            },
+          ],
+        },
+        linux: {
+          icon: "./src/assets/app256x256.png",
+          category: "Network",
+          target: ["deb"],
+          artifactName: "${productName}.v${version}.${ext}",
+        },
+      },
+    },
+  },
 })
